8fb86949a9be71ffe3120b374fac2f3b9183be1b
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-06-29 17:57:13 +0600

    ADded task system

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,6 +1,18 @@
+import { postTask } from "./task";
+
 export const createATask = async (formData) => {
   "use server";
 
-  const name = formData.get("name");
-  console.log("Adding a task with name: ", formData);
+  // const title = formData.get("title");
+  // const description = formData.get("description");
+  // const prority = formData.get("priority");
+  // const status = formData.get("status");
+  // const assignedTo = formData.get("assignedTo");
+
+  // const newTaks = { title, description, prority, status, assignedTo };
+
+  const newTask = Object.fromEntries(formData.entries());
+  console.log("Addig a task with new tassk: ", newTask);
+
+  await postTask(newTask);
 };
--- src/lib/task.js
+++ src/lib/task.js
@@ -2,3 +2,10 @@ import tasks from "../data/tasks.json";
 export const getTasks = async () => {
   return tasks;
 };
+
+export const postTask = async (newTask) => {
+  newTask.id = tasks.length + 1;
+
+  tasks.push(newTask);
+  return { success: true, message: "Task added successfully" };
+};
